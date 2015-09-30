6bfec6975d4b8a3a395d8b31178dae23c75921fe
voltrue2 <voltrue2@gmail.com>
2015-09-30 14:00:25 +0900

    non-select query on slave does not throw exception

--- model/src/sql.class.php
+++ model/src/sql.class.php
@@ -32,7 +32,7 @@ class Sql {
 	public function read($sql, $params = array()) {
 
 		if (strpos(strtolower($sql), 'select') !== 0) {
-			throw new Exception('Invalid SQL query for [read]: query must be a SELECT statement: ' . $sql);
+			$this->console->error('Invalid SQL query for [read]: query must be a SELECT statement: ' . $sql);
 			return array();
 		}
 
